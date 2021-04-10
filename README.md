9023efe1e66989cdfe940f81f2ef1b017abece20
Brody Rethy <brodyrethy@gmail.com>
2021-04-10 20:03:20 -0300

    improved keybindings

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 My build of st. Minimally patched. Contains transparency, and scrollback patches.
 
 Keybindings are as follows:
-- **Alt-Shift-K/J**: Font size +1/-1
-- **Alt-ShiftMask-Home**: Reset font size
-- **Alt-k/j**: Scroll back/up
+- **Alt-minus/equal**: Font size +1/-1
+- **Alt-ShiftMask-Plus**: Reset font size
+- **Alt-j/k**: Scroll back/up
 - **Ctrl-Shift-Y**: Paste
 - **Ctrl-Shift-c/v**: Copy/paste
 - **Shift-Insert**: Paste
--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus (TTF):size=9:antialias=true:autohint=true";
+static char *font = "Terminus (TTF):pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -195,9 +195,9 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_K,           zoom,           {.f = +1} },
-	{ MODKEY|ShiftMask,     XK_J,		    zoom,           {.f = -1} },
-	{ MODKEY|ShiftMask,     XK_Home,        zoomreset,      {.f =  0} },
+	{ MODKEY,				XK_equal,       zoom,           {.f = +1} },
+	{ MODKEY,			    XK_minus,	    zoom,           {.f = -1} },
+	{ MODKEY|ShiftMask,     XK_plus,		zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
